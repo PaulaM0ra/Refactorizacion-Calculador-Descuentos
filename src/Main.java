960e70fe22f6ecd92034fa85e3787b519cb83d79
PaulaM0ra <117874193+PaulaM0ra@users.noreply.github.com>
2026-09-17 11:43:13 -0500

    "test: comprobar funcionamiento de las estrategias"

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ProcesoCompra procesoCompra =
+                new ProcesoCompra();
+
+        double valorCompra = 100000;
+
+        double descuentoFrecuente =
+                procesoCompra.realizarCompra(
+                        valorCompra,
+                        new DescuentoFrecuente());
+
+        double descuentoTemporada =
+                procesoCompra.realizarCompra(
+                        valorCompra,
+                        new DescuentoTemporadaBaja());
+
+        double descuentoConvenio =
+                procesoCompra.realizarCompra(
+                        valorCompra,
+                        new DescuentoConvenio());
+
+        System.out.println(
+                "Descuento frecuente: "
+                        + descuentoFrecuente);
+
+        System.out.println(
+                "Descuento temporada baja: "
+                        + descuentoTemporada);
+
+        System.out.println(
+                "Descuento convenio: "
+                        + descuentoConvenio);
+    }
+}
